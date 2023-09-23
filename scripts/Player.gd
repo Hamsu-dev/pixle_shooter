88c08126db2c8bc8dfc6d1ef796369e582da430a
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-09-24 12:53:13 +1300

    huge updates

---
+++ scripts/Player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+@onready var weapons = $Weapons
+@onready var sprite_2d = $Weapons/Sprite2D
+
+const speed = 200  # Adjust the speed as needed
+const gun_radius = 25 # Adjust the radius of the circular range
+
+func _physics_process(_delta):
+	var movement_direction = Vector2.ZERO
+
+	# Input handling for movement
+	if Input.is_action_pressed("left"):
+		movement_direction.x -= 1
+	if Input.is_action_pressed("right"):
+		movement_direction.x += 1
+	if Input.is_action_pressed("up"):
+		movement_direction.y -= 1
+	if Input.is_action_pressed("down"):
+		movement_direction.y += 1
+
+	movement_direction = movement_direction.normalized()
+	velocity = movement_direction * speed
+	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		# Get the mouse position in global coordinates
+		var mouse_pos = get_global_mouse_position()
+
+		# Calculate the direction from the character to the mouse
+		var direction = (mouse_pos - global_position).normalized()
+
+		# Calculate the gun position within the circular range
+		var gun_position = global_position + direction * gun_radius
+
+		# Set the gun's position relative to the character
+		weapons.global_position = gun_position
+
+		# Look at the mouse position
+		weapons.look_at(mouse_pos)
+
+		if mouse_pos.x < weapons.global_position.x:
+			sprite_2d.scale = Vector2(1, -1)
+		elif mouse_pos.x > weapons.global_position.x:
+			sprite_2d.scale = Vector2(1, 1)
--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+@onready var marker_2d = $Sprite2D/Marker2D
 @onready var bulletScene = preload("res://scenes/Bullet.tscn")
 var mouse_pressed = false
 
@@ -12,8 +12,9 @@ func _unhandled_input(_event):
 		
 	if Input.is_action_just_released('fire') and mouse_pressed:
 		var bullet = bulletScene.instantiate() as Node2D
-		get_parent().add_child(bullet)
-		bullet.global_position = self.global_position
+		var root = get_tree().get_root() 
+		root.add_child(bullet)
+		bullet.global_position = marker_2d.global_position
 		bullet.direction = (get_global_mouse_position() - global_position).normalized()
 		bullet.rotation = bullet.direction.angle()
 		mouse_pressed = false
